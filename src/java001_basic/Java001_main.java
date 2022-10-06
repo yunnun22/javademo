18d4b8bd2ae1c49aff77c631d8b3b7bebc12ee8a
yun <yunnun5548@gmail.com>
2022-10-06 10:40:09 +0900

    ss

---
+++ src/java001_basic/Java001_main.java
@@ -0,0 +1,7 @@
+package java001_basic;
+
+public class Java001_main {
+	public static void main (String [] args) {
+	System.out.println ("hello java start~~");   
+	}
+}
---
+++ src/java001_basic/Java002_comment.java
@@ -0,0 +1,22 @@
+package java001_basic; 
+
+public class Java002_comment {
+	public static void main (String [] args) { 
+	     //한 라인 주석
+		/*
+		 * 여러 라인 주석
+		 * 
+		 */
+		 
+		/**
+		 * java doc 주석: 프로그램 문서를 만들때 사용하는 주석
+		 * 
+		 */
+		
+		//여기에서 3은 각자 다른 메모리에 저장된다. 한번만 사용할 수 있다.
+		System.out.println(3);
+		System.out.println(3);
+		System.out.println(3);
+	}
+}
+
