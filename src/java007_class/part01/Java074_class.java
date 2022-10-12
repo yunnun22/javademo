ed6570b463faab0950b539d5859d80d577ecd952
yun <yunnun5548@gmail.com>
2022-10-12 18:10:04 +0900

    20221012

--- src/java007_class/part01/Java074_class.java
+++ src/java007_class/part01/Java074_class.java
@@ -1,19 +1,34 @@
 package java007_class.part01;
 
 //"Nikon",400000,30,50
-//"sony",450000,20,35
+//"Sony",450000,20,35
 //"Fujifilm",35000,10,25
 public class Java074_class {
 
 	public static void main(String[] args) {
-		Goods g1 = new Goods ("Nikon",400000,30,50);
-		g1.display();
-
-		Goods g2 = new Goods ("sony",450000,20,35);
-		g2.display();
+//		Goods g1 = new Goods ("Nikon",400000,30,50);
+//		g1.display();
+//
+//		Goods g2 = new Goods ("Sony",450000,20,35);
+//		g2.display();
+//		
+//		Goods g3 = new Goods ("Fujifilm",35000,10,25);
+//		g3.display();
 		
-		Goods g3 = new Goods ("Fujifilm",35000,10,25);
-		g3.display();
+		Goods [] goods = new Goods[3];
+		goods [0]= new Goods ("Nikon",400000,30,50);
+		goods [1]= new Goods ("Sony",450000,20,35);
+		goods [2]= new Goods ("Fujifilm",35000,10,25);
+//		
+//		goods [0].display();
+//		goods [1].display();
+//		goods [2].display();
+		
+		for (int i= 0; i<goods.length; i++) {
+			goods[i].display();
+		}
+		
+		
 	}
 
 }
