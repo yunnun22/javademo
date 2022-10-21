c472cf858ceb2c7ebab952ba6188f2c8034e564b
yun <yunnun5548@gmail.com>
2022-10-21 17:36:59 +0900

    20221021

--- src/java017_collection/Java183_Vector.java
+++ src/java017_collection/Java183_Vector.java
@@ -1,5 +1,8 @@
 package java017_collection;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.Vector;
 
 /*
@@ -16,15 +19,29 @@ public class Java183_Vector {
       prnDisplay(vt);
    }// end main()
 
-   private static Vector<Sawon> lines(String filename) {
+   private static Vector<Sawon> lines(String fileName) {
       // strName 매개변수의 값을 이용해서 Vector에 데이터를 저장한 후
       // 리턴하는 프로그램을 구현하시오.
-      return null;
+	   Vector<Sawon> vt = new Vector<Sawon>();
+	   try (Scanner sc = new Scanner(new File(fileName))){
+		   while(sc.hasNextLine()) {//kim:56/78/12
+			   String[]data=sc.nextLine().split("[:/]");
+			   Sawon sn = new Sawon(data[0],Integer.parseInt(data[1]),
+					   Integer.parseInt(data[2]),Integer.parseInt(data[3] ));
+			   vt.add(sn);
+			   
+		   }
+	   }catch(FileNotFoundException ex) {
+		   System.out.println(ex.toString());
+	   }
+      return vt;
    }
 
    private static void prnDisplay(Vector<Sawon> vt) {
       // vt에 저장된 객체들을 출력결과와 같이 프로그램을 구현하시오.
-
-   }
+   
+   for(Sawon sn : vt) {
+	   System.out.println(sn.toString());
+   }}
 
 }// end class
